01e33a05fcb6fdcc8f79f8cffda3acb6efcd8876
Martin <refshet@yahoo.no>
2017-07-06 20:38:01 +0200

    0.3

--- bot.py
+++ bot.py
@@ -40,20 +40,20 @@ class IceBot(discord.Client):
         self.player = None
         self.voice = None
 
-    url = streamurl  # radio stream
-    encoding = 'latin1' # default: iso-8859-1 for mp3 and utf-8 for ogg streams
-    request = urllib2.Request(url, headers={'Icy-MetaData': 1})  # request metadata
+    url = streamurl
+    encoding = 'latin1'
+    request = urllib2.Request(url, headers={'Icy-MetaData': 1})
     response = urllib2.urlopen(request)
     metaint = int(response.headers['icy-metaint'])
-    for _ in range(10): # # title may be empty initially, try several times
-        response.read(metaint)  # skip to metadata
-        metadata_length = struct.unpack('B', response.read(1))[0] * 16  # length byte
+    for _ in range(10):
+        response.read(metaint)
+        metadata_length = struct.unpack('B', response.read(1))[0] * 16
         metadata = response.read(metadata_length).rstrip(b'\0')
-        # extract title from the metadata
         m = re.search(br"StreamTitle='([^']*)';", metadata)
         if m:
             title = m.group(1)
             nowPlayingDecode = title.decode()
+            global nowPlaying
             nowPlaying = nowPlayingDecode.replace("b'", "'")
         if title:
             break
@@ -69,22 +69,16 @@ class IceBot(discord.Client):
         print('===========================================')
         print("")
         Vchannel = self.get_channel(Vc)
-        Tchannel = self.get_channel(Tc)
         await self.join_voice_channel(Vchannel)
 
     async def on_message(self, message):
-        if message.content.startswith(pfx + 'count'):
-            counter = 0
-            tmp = await self.send_message(message.channel, 'Calculating messages...')
-            async for log in self.logs_from(message.channel, limit=100):
-                if log.author == message.author:
-                    counter += 1
-            await self.edit_message(tmp, 'You have {} messages.'.format(counter))
+        if message.content.startswith(pfx + 'playing'):
+            await self.send_message(message.channel, nowPlaying)
 
         elif message.content.startswith(pfx + 'stream'):
             await self.send_message(message.channel, 'Now Streaming ' + streamurl)
             self.voice = self.voice_client_in(message.server)
-            self.player = self.voice.create_stream_player(streamurl)
+            self.player = self.voice.create_stream_player(streamurl)  #PCM Frames
             self.player.start()
 
         elif message.content.startswith(pfx + "rick"):
